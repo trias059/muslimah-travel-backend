68265e568cbcbad9cd6f5f5c6ce66892fc0b7d96
lomanto215 <triasajilomanto215@gmail.com>
2025-12-05 19:13:57 +0700

    update

--- src/config/db.js
+++ src/config/db.js
@@ -2,13 +2,25 @@ import pg from 'pg';
 
 const { Pool } = pg;
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL 
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        },
+        max: 10, 
+        idleTimeoutMillis: 30000,
+        connectionTimeoutMillis: 2000,
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+      }
+);
 
 pool.on('connect', () => {
   console.log('Database connected');
